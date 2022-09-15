5eec15ab01d22b4647412013ba3633075f1b97cc
Conor Gleeson <conorgleeson2001@gmail.com>
2022-09-15 12:35:01 +0100

    loop constructs

--- fundamentals.js
+++ fundamentals.js
@@ -18,7 +18,8 @@ const aCar = {
         county: "WD",
         number: "1058"
         
-    }
+    },
+    features : ['Parking assist', 'Alarm', 'Tow-bar'],
   };
 
   aCar.mileage = "100000" ;
@@ -42,3 +43,20 @@ const aCar = {
   console.log("Milage = " + aCar.mileage)
   console.log("It is a " + aCar.colour.paint + " car, " + aCar.mileage + " mileage, " + " with " + aCar.colour.interior.texture + " interior" )
   console.log('First owner : ' + aCar.previous_owners[0].name + " " + aCar.previous_owners[0].address )
+
+  for (let i = 0 ; i < aCar.features.length ; i += 1) {
+    console.log(aCar.features[i]) ;
+} 
+
+for (let i = 0 ; i < aCar.previous_owners.length ; i += 1) {
+    console.log(aCar.previous_owners[i].name) ;
+
+} 
+
+for (let p in aCar.type)  {
+    console.log(p.toUpperCase() + ' = ' + aCar.type[p] ) ;
+}
+
+// for(const i of previous_owners){
+//     console.log(aCar.previous_owners[i].name);
+// }
