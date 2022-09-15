c446ab86d019fe52dcc26ef41a6f92d6d2745350
Conor Gleeson <conorgleeson2001@gmail.com>
2022-09-15 12:08:55 +0100

    Dynamic Properties

--- fundamentals.js
+++ fundamentals.js
@@ -13,8 +13,24 @@ const aCar = {
         
     }
   };
+
+  aCar.mileage = "100000" ;
+
+  aCar.colour ={
+    paint: "red",
+    interior: {
+        texture: "Leather",
+        colour: "Cream"
+    }
+    
+  }
+
+  
+
+
   
   console.log(aCar.owner);
   console.log(aCar.owner + ' drives a ' + aCar.type.make)
   console.log("Reg. = " + aCar.registration.year +"-"+ aCar.registration.county + "-" + aCar.registration.number )
-  
+  console.log("Milage = " + aCar.mileage)
+  console.log("It is a " + aCar.colour.paint + " car, " + aCar.mileage + " mileage, " + " with " + aCar.colour.interior.texture + " interior" )
